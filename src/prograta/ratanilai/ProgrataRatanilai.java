36addda9f364fb961dfe28cdd711100e8bacdeaa
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-10-15 19:20:42 +0700

    Add files via upload

---
+++ src/prograta/ratanilai/ProgrataRatanilai.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prograta.ratanilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 'KoalaX'
+ * NAMA     : Fiqri Akbar Pratama
+ * KELAS    : PBO1/IF1
+ * NIM      : 10118020
+ * Deksripsi Program : Program ini berisi program untuk menampilkan nilai 
+ * rata-rata dari beberapa siswa/siswi
+ */
+public class ProgrataRatanilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        Nilai nilai1 = new Nilai();
+        System.out.print("Masukkan Banyaknya Mahasiswa : ");
+        nilai1.jumlahSiswa = scanner.nextInt();
+        nilai1.inputHitungNilai(nilai1.jumlahSiswa, nilai1.nilai);
+        System.out.println("Maka, Rata-rata Nilainya adalah : " + nilai1.rataRataNilai());
+        System.out.println("\nDevelop by : Fiqri Akbar Pratama");
+    }
+    
+}
